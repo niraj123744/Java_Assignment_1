69fdd3c585e93c7625cda4812c9624f214f4d15e
niraj123744 <nirajnirmal661@gmail.com>
2023-04-13 21:40:59 +0530

    [Niraj] Add: This script is used for creating program Command line

---
+++ Command_line.java
@@ -0,0 +1,9 @@
+package com.mypackage;
+
+public class Command_line {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Your first argument is: " + args[0]);
+	}
+}
